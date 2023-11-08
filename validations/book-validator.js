b1b4da53f59ad10dfc036abcb870930f524c9bb3
KhalilAbdulKarim <khalil_sasha@hotmail.com>
2023-11-08 12:18:54 +0200

    validation

---
+++ validations/book-validator.js
@@ -0,0 +1,31 @@
+const {check} = require("express-validator");
+
+const createBookValidation = [
+    check('title').notEmpty().withMessage('Title is required'),
+    check('publishedDate').isDate().withMessage('Date must be valid'),
+    check('ISBN').isISBN(13).withMessage('Invalid ISBN-13'),
+    check('genreID').isInt(),
+    check('genreID').notEmpty().withMessage('Genre ID is required'),
+    check('authorID').isInt(),
+    check('authorID').notEmpty().withMessage('Author ID is required')
+];
+
+const updateBookValidation = [
+    check('bookID').isInt(),
+    check('bookID').notEmpty().withMessage('Book ID is required'),
+    check('title').notEmpty().withMessage('Title is required'),
+    check('ISBN').isISBN(13).withMessage('Invalid ISBN-13'),
+    check('ISBN').notEmpty().withMessage('ISBN is required'),
+    check('genreID').isInt(),
+    check('genreID').notEmpty().withMessage('Genre ID is required'),
+    check('publishedDate').isDate().withMessage('Date must be valid'),
+    check ('publishedDate').notEmpty().withMessage('Date cannot be empty')
+
+];
+
+module.exports = {
+    createBookValidation,
+    updateBookValidation
+}
+
+
